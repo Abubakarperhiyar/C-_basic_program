1561a727938e3754d0908906c86604fab4b5ba7d
Abubakarperhiyar <abubakarperhiyar06@gmail.com>
2024-09-23 00:28:19 -0700

    adding assingment

---
+++ sir assingment 01.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout<<"sir mazhar memon\n teacher of programming fundamental"<<endl; //checking for new line 
+	
+	cout<<"\t physics"<<endl;//checking 5spaces,tab.
+	
+	cout<<"abubakar zulfiqar\a"<<endl;//checking 
+	
+	cout<<"crimnology\\justify"<<endl;//backslash
+	
+	cout<<"\'money is important\'"<<endl;//single quote.
+	
+	cout<<"\"work,work and work\"";//double quote.
+	return 0;
+}
---
+++ sir assingment 02.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout<<"subject\t\t\tmarks"<<endl;
+	cout<<"functional english\t75"<<endl;
+	cout<<"islamiyat\t\t86"<<endl;
+	cout<<"calculus\t\t89"<<endl;
+	cout<<"pakistan study\t\t63"<<endl;
+	cout<<"applied physics\t\t69";
+	
+	
+	return 0;
+}
